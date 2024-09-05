21b5d9d4ac9334129c684abdb6ad72f547d65156
Soura Nemoto <n.soura.official@gmail.com>
2024-09-05 20:36:18 +0900

    add useState hook

--- address-converter/src/form/index.tsx
+++ address-converter/src/form/index.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react';
+
+interface State {
+    postalCode: string;
+}
+
 const FormComponent = (): JSX.Element => {
+    const [state, setState] = useState<State>({
+        postalCode:"",
+    });
+
+    const handlers = {
+        input: (e: React.ChangeEvent<HTMLInputElement>) => {
+            setState({
+                ...state,
+                postalCode: e.target.value,
+            });
+        },
+        submit: (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            console.log(state);
+        },
+    };
     
-    function search(formData) {
-        const query = formData.get("query");
-        alert(`You searched for '${query}'`);
-    }
     return (
-        <form action={search}>
-            <input name="query" />
-            <button type="submit">Submit</button>
+        <form
+            onSubmit={handlers.submit}
+        >
+            <label>
+                郵便番号
+            </label>
+            <input
+                type='text'
+                value={state.postalCode}
+                onChange={handlers.input}
+            />
+            <button type='submit'>
+                住所検索
+            </button>
         </form>
     );
-};
+  };
   
 export default FormComponent;
-  
